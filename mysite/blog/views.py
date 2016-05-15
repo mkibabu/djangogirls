5b9179e3f63985db6579528b8a3b3ef033cc6a61
Michael Muraya <mkibabu@gmail.com>
2016-05-15 08:03:32 -0400

    Add first view and template

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Views are merely functions that are called when a valid request is received.
+# If a requests's url matches a pattern-view pair in the urls.py file, the corresponding
+# view function is executed.
+
+def post_list(request):
+    return render(request, 'blog/post_list.html')
