51b90a3afa169dd1fcdb77ce87fa6dd95f6604ec
Michael Muraya <mkibabu@gmail.com>
2016-05-29 23:58:07 -0400

    Add post-specific view and template

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -10,4 +10,8 @@ and makes the blog application more encapsilated and easier to copy elsewhere.
 urlpatterns = [
     #url(r'pattern', view_to_handle_request, name='a name for the view')
     url(r'^$', views.post_list, name='post_list'),
+    # here, match a url with the pattern 'post/NNN..', where NNN.. are digits.
+    # the digits after the 'post/' are to be passed to the view as a variable 'post_id'.
+    # pattern: starts with "post/", is followed by gte 1 digit(s) and ends.
+    url(r'^post/(?P<post_id>\d+)/$', views.post_detail, name='post_detail')
 ]
